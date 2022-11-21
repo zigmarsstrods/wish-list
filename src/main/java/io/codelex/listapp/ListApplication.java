ac8d3da0bfa6b26bbba6d9a196d08507cd2da94e
Zigmars Strods <zigmars.strods@gmail.com>
2022-11-22 01:27:56 +0200

    Added API endpoint description with Swagger

--- src/main/java/io/codelex/listapp/ListApplication.java
+++ src/main/java/io/codelex/listapp/ListApplication.java
@@ -3,11 +3,11 @@ package io.codelex.listapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ListApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ListApplication.class, args);
     }
-
 }
--- src/main/java/io/codelex/listapp/users/UsersController.java
+++ src/main/java/io/codelex/listapp/users/UsersController.java
@@ -1,6 +1,7 @@
 package io.codelex.listapp.users;
 
 import io.codelex.listapp.users.domain.UserList;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +14,7 @@ public class UsersController {
         this.usersService = usersService;
     }
 
+    @ApiOperation(value = "To get names for all users provided in the input JSON array")
     @PutMapping("/get-all-names")
     public String getAllNames(@RequestBody UserList users) {
         return usersService.getAllNames(users);
